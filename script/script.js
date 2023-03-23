f56065fa445d8ce06148a578b24e1aa4aee3eba8
satyabratait <satyabrata@itobuz.com>
2023-03-23 15:11:37 +0530

    sixth commit

--- script/script.js
+++ script/script.js
@@ -1,8 +1,7 @@
 const locationname = document.getElementById("location-name");
 let locationtemp = document.getElementById("location-temp");
 let locationfeels = document.getElementById("location-feels");
-let locationother = document.getElementById("location-other");
-let locationimage = document.querySelector(".location-info__image");
+let locationimage = document.querySelector(".locationInfoImage");
 
 let weathersuggestion = document.getElementById("weatherSuggestion");
 
@@ -11,7 +10,6 @@ let obj = [];
 async function weather(query) {
   const response = await fetch(
     `http://127.0.0.1:5050/get?place=${query}`
-    // `http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=${query}&aqi=no`
   )
   .then(function (res) {
     // console.log(res.json());
@@ -27,55 +25,62 @@ async function weather(query) {
     
   obj.push(responsecities)  
   suggestions();
-  locationname.value = `${response.location.name}`;
-  locationtemp.innerHTML = response.current.tempC;
-  locationfeels.innerHTML = response.current.tempF;
-
-  let conditionvalue = response.current.condition.text;
-
-  if (response.current.is_day === 1) {
-    if (conditionvalue === "Sunny" || conditionvalue === "Clear") {
-      locationimage.src = "./assets/weather/weather-svg/animated/day.svg";
-    } else if (
-      conditionvalue === "Partly cloudy" ||
-      conditionvalue === "Overcast"
-    ) {
-      locationimage.src =
-        "./assets/weather/weather-svg/animated/cloudy-day-1.svg";
-    } else if (conditionvalue === "Cloudy") {
-      locationimage.src = "./assets/weather/weather-svg/animated/cloudy.svg";
-    } else if (conditionvalue === "Rain" || conditionvalue === "Moderate rain") {
-      locationimage.src = "./assets/weather/weather-svg/animated/rainy-4.svg";
-    } else if (conditionvalue === "Heavy rain") {
-      locationimage.src = "./assets/weather/weather-svg/animated/rainy-7.svg";
-    } else if (conditionvalue === "Mist" || conditionvalue === "Moderate snow") {
-      locationimage.src = "./assets/weather/weather-svg/animated/snowy-2.svg";
-    } else if (conditionvalue === "Snow") {
-      locationimage.src = "./assets/weather/weather-svg/animated/snowy-6.svg";
+  try {
+    locationname.value = `${response.location.name}`;
+    locationtemp.innerHTML = response.current.tempC;
+    locationfeels.innerHTML = response.current.tempF;
+  
+    let conditionvalue = response.current.condition.text;
+  
+    if (response.current.is_day === 1) {
+      if (conditionvalue === "Sunny" || conditionvalue === "Clear") {
+        locationimage.src = "./assets/weather/weather-svg/animated/day.svg";
+      } else if (
+        conditionvalue === "Partly cloudy" ||
+        conditionvalue === "Overcast"
+      ) {
+        locationimage.src =
+          "./assets/weather/weather-svg/animated/cloudy-day-1.svg";
+      } else if (conditionvalue === "Cloudy") {
+        locationimage.src = "./assets/weather/weather-svg/animated/cloudy.svg";
+      } else if (conditionvalue === "Rain" || conditionvalue === "Moderate rain") {
+        locationimage.src = "./assets/weather/weather-svg/animated/rainy-4.svg";
+      } else if (conditionvalue === "Heavy rain") {
+        locationimage.src = "./assets/weather/weather-svg/animated/rainy-7.svg";
+      } else if (conditionvalue === "Mist" || conditionvalue === "Moderate snow") {
+        locationimage.src = "./assets/weather/weather-svg/animated/snowy-2.svg";
+      } else if (conditionvalue === "Snow") {
+        locationimage.src = "./assets/weather/weather-svg/animated/snowy-6.svg";
+      }
+    } else {
+      if (conditionvalue === "Clear") {
+        locationimage.src = "./assets/weather/weather-svg/animated/night.svg";
+      } else if (
+        conditionvalue === "Partly cloudy" ||
+        conditionvalue === "Overcast"
+      ) {
+        locationimage.src =
+          "./assets/weather/weather-svg/animated/cloudy-night-1.svg";
+      } else if (conditionvalue === "Cloudy") {
+        locationimage.src =
+          "./assets/weather/weather-svg/animated/cloudy-night-3.svg";
+      } else if (conditionvalue === "Rain" || conditionvalue === "Moderate rain") {
+        locationimage.src = "./assets/weather/weather-svg/animated/rainy-4.svg";
+      } else if (conditionvalue === "Heavy rain") {
+        locationimage.src = "./assets/weather/weather-svg/animated/rainy-7.svg";
+      } else if (conditionvalue === "Mist" || conditionvalue === "Moderate snow" || conditionvalue === "Light snow") {
+        locationimage.src = "./assets/weather/weather-svg/animated/snowy-4.svg";
+      } else if (conditionvalue === "Snow" || conditionvalue === "Heavy snow") {
+        locationimage.src = "./assets/weather/weather-svg/animated/snowy-6.svg";
+      }
     }
-  } else {
-    if (conditionvalue === "Clear") {
-      locationimage.src = "./assets/weather/weather-svg/animated/night.svg";
-    } else if (
-      conditionvalue === "Partly cloudy" ||
-      conditionvalue === "Overcast"
-    ) {
-      locationimage.src =
-        "./assets/weather/weather-svg/animated/cloudy-night-1.svg";
-    } else if (conditionvalue === "Cloudy") {
-      locationimage.src =
-        "./assets/weather/weather-svg/animated/cloudy-night-3.svg";
-    } else if (conditionvalue === "Rain" || conditionvalue === "Moderate rain") {
-      locationimage.src = "./assets/weather/weather-svg/animated/rainy-4.svg";
-    } else if (conditionvalue === "Heavy rain") {
-      locationimage.src = "./assets/weather/weather-svg/animated/rainy-7.svg";
-    } else if (conditionvalue === "Mist" || conditionvalue === "Moderate snow" || conditionvalue === "Light snow") {
-      locationimage.src = "./assets/weather/weather-svg/animated/snowy-4.svg";
-    } else if (conditionvalue === "Snow" || conditionvalue === "Heavy snow") {
-      locationimage.src = "./assets/weather/weather-svg/animated/snowy-6.svg";
-    }
+    console.log(response); 
   }
-  console.log(response);
+  catch(err){
+    console.error(err);
+    alert("Entered wrong location");
+    weather("Bhubaneswar");
+  }
 }
 
 function suggestions() {
